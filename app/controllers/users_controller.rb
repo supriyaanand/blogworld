0dafd4b8b67a5448f1be4b7a57335d74755a2b09
supriyaanand <supriya.anandrao@gmail.com>
2015-08-20 15:17:31 -0700

    Init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
+    def new
+      @user = User.new
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        redirect_to 'root'
+      else
+        render 'new'
+      end
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def index
+      @users = User.all
+    end
+
+    def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+
+      redirect_to users_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+
+end
